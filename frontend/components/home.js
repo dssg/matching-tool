3a5bfd24b93a34a193a6b1f12dc1a3d0717f30e7
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-08-10 10:35:48 -0500

    Material UI

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default React.createClass({
   render: function() {
     return (
       <div>
-        Home
+        <h2>Integrating HMIS and Criminal Justice Data</h2>
+        <div>
+          <Card style={{width: '25%'}}>
+            <CardTitle title='Upload' />
+            <CardText>
+              Some text here explaining something about uploading
+            </CardText>
+          </Card>
+          <Card style={{width: '25%'}}>
+            <CardTitle title='Charts' />
+            <CardText>
+              Some text here explaining something about charts
+            </CardText>
+          </Card>
+        </div>
       </div>
     )
   }
