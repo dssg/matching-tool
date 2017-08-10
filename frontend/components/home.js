72cb6771ee4f17238466ebe732845a2d99e71388
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-08-10 11:13:06 -0500

    Fix layout

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -2,22 +2,36 @@ import React from 'react'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+
+const styles = {
+  page: {
+    margin: '50px'
+  },
+  container: {
+    display: 'flex',
+    'justify-content': 'space-between',
+    width: '50%'
+  },
+  card: {
+    width: '40%',
+  }
+}
 export default React.createClass({
   render: function() {
     return (
-      <div>
+      <div style={styles.page}>
         <h2>Integrating HMIS and Criminal Justice Data</h2>
-        <div>
-          <Card style={{width: '25%'}}>
+        <div style={styles.container}>
+          <Card style={styles.card}>
             <CardTitle title='Upload' />
             <CardText>
-              Some text here explaining something about uploading
+              Some text here explaining something about uploading Some text here explaining something about uploading Some text here explaining something about uploading
             </CardText>
           </Card>
-          <Card style={{width: '25%'}}>
+          <Card style={styles.card}>
             <CardTitle title='Charts' />
             <CardText>
-              Some text here explaining something about charts
+              Some text here explaining something about charts Some text here explaining something about charts Some text here explaining something about charts
             </CardText>
           </Card>
         </div>
