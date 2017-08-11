5276d2a546028b10264170a68ea28c903493c9e8
tweddielin <tweddielin@gmail.com>
2017-08-11 10:53:36 -0500

    create charts page for dashboard

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Charts from './charts'
 import Home from './home'
 import Upload from './upload'
 import UploadSuccess from './upload-success'
@@ -46,6 +47,7 @@ export default class App extends React.Component {
             <Tabs style={styles.tabs} value={this.state.value} onChange={this.handleChange}>
               <Tab value={0} label="Home" containerElement={<Link to="/"/>} />
               <Tab value={1} label="Upload Data" containerElement={<Link to="/upload"/>}/>
+              <Tab value={2} label="Charts" containerElement={<Link to="/charts"/>}/>
             </Tabs>
           </AppBar>
           <Switch>
@@ -53,6 +55,7 @@ export default class App extends React.Component {
             <Route path='/upload' component={Upload}/>
             <Route path='/upload_success' component={UploadSuccess} />
             <Route path='/upload_invalid' component={UploadInvalid} />
+            <Route path='/charts' component={Charts}/>
           </Switch>
         </div>
       </MuiThemeProvider>
