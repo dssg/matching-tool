5ec814c6d9d35c585fa7c8433b292361308ebafc
tweddielin <tweddielin@gmail.com>
2017-08-23 17:00:44 -0500

    add more columns, duration bar for homeless, and download buttons

--- frontend/components/charts.js
+++ frontend/components/charts.js
@@ -4,6 +4,7 @@ import DurationBarChart from './bar'
 import Venn from './venn'
 import TableList from './table'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 
 
@@ -16,12 +17,21 @@ const styles = {
     display: 'flex',
     'justify-content': 'space-between',
   },
+  venn: {
+    width: '70%',
+  },
   card: {
-    width: '100%',
+    width: '120%',
+  },
+  bar_chart: {
+    width: '60%',
   },
   list: {
     'text-align': 'center',
   },
+  button: {
+    margin: '12',
+  }
 }
 export default class Charts extends React.Component {
   constructor(props) {
@@ -29,24 +39,112 @@ export default class Charts extends React.Component {
     this.state = {
       data: [ {sets: ['Jail'], size: 500}, {sets: ['Homeless'], size: 340}, {sets: ['Jail','Homeless'], size: 100}],
       table_data: [
-                { ID: 12319, Name: 'Roy Batty', Source: ['Jail', 'Homeless'], Duration: 10, Count: 5 },
-                { ID: 17144, Name: 'James Moriarty', Source: ['Jail', 'Homeless'], Duration: 18, Count: 8 },
-                { ID: 12432, Name: 'Jason Smith', Source: 'Homeless', Duration: 1, Count: 1 },
-                { ID: 19332, Name: 'H. H. Holmes', Source: 'Jail', Duration: 241, Count: 1 },
-                { ID: 19032, Name: 'Jack Ripper', Source: 'Jail', Duration: 102, Count: 2 },
-                { ID: 12143, Name: 'Lee Salminen', Source: 'Homeless', Duration: 43, Count: 5 },
-                { ID: 12833, Name: 'John Doe', Source: ['Jail', 'Homeless'], Duration: 10, Count: 3 },
-                { ID: 13833, Name: 'Jane Doe', Source: 'Homeless', Duration: 1, Count: 2 },
-                { ID: 13932, Name: 'Evan Jackson', Source: ['Jail', 'Homeless'], Duration: 5, Count: 1 },
-                { ID: 19982, Name: 'Liam Smith', Source: 'Homeless', Duration: 7, Count: 3 },
-                { ID: 19932, Name: 'Griffin Smith', Source: 'Homeless', Duration: 3, Count: 2 },
+                { ID: 12319,
+                  Name: 'Roy Batty',
+                  Source: ['Jail', 'Homeless'],
+                  'Total Duration': 10,
+                  'Total Counts': 5,
+                  '# of Jail Days': 8,
+                  '# of Jail Counts': 3,
+                  '# of Homeless Days': 2,
+                  '# of Homeless Counts': 2 },
+                { ID: 17144,
+                  Name: 'James Moriarty',
+                  Source: ['Jail', 'Homeless'],
+                  'Total Duration': 18,
+                  'Total Counts': 8,
+                  '# of Jail Days': 16,
+                  '# of Jail Counts': 5,
+                  '# of Homeless Days': 2,
+                  '# of Homeless Counts': 3 },
+                { ID: 12432,
+                  Name: 'Jason Smith',
+                  Source: 'Homeless',
+                  'Total Duration': 1,
+                  'Total Counts': 1,
+                  '# of Jail Days': 0,
+                  '# of Jail Counts': 0,
+                  '# of Homeless Days': 1,
+                  '# of Homeless Counts': 2 },
+                { ID: 19332,
+                  Name: 'H. H. Holmes',
+                  Source: 'Jail',
+                  'Total Duration': 241,
+                  'Total Counts': 1,
+                  '# of Jail Days': 241,
+                  '# of Jail Counts': 1,
+                  '# of Homeless Days': 0,
+                  '# of Homeless Counts': 0 },
+                { ID: 19032,
+                  Name: 'Jack Ripper',
+                  Source: 'Jail',
+                  'Total Duration': 102,
+                  'Total Counts': 2,
+                  '# of Jail Days': 102,
+                  '# of Jail Counts': 2,
+                  '# of Homeless Days': 0,
+                  '# of Homeless Counts': 0 },
+                { ID: 12143,
+                  Name: 'Lee Salminen',
+                  Source: 'Homeless',
+                  'Total Duration': 43,
+                  'Total Counts': 5,
+                  '# of Jail Days': 0,
+                  '# of Jail Counts': 0,
+                  '# of Homeless Days': 43,
+                  '# of Homeless Counts': 5 },
+                { ID: 12833,
+                  Name: 'John Doe',
+                  Source: ['Jail', 'Homeless'],
+                  'Total Duration': 10,
+                  'Total Counts': 3,
+                  '# of Jail Days': 2,
+                  '# of Jail Counts': 1,
+                  '# of Homeless Days': 8,
+                  '# of Homeless Counts': 2 },
+                { ID: 13833,
+                  Name: 'Jane Doe',
+                  Source: 'Homeless',
+                  'Total Duration': 2,
+                  'Total Counts': 2,
+                  '# of Jail Days': 0,
+                  '# of Jail Counts': 0,
+                  '# of Homeless Days': 2,
+                  '# of Homeless Counts': 2 },
+                { ID: 13932,
+                  Name: 'Evan Jackson',
+                  Source: ['Jail', 'Homeless'],
+                  'Total Duration': 5,
+                  'Total Counts': 2,
+                  '# of Jail Days': 1,
+                  '# of Jail Counts': 1,
+                  '# of Homeless Days': 4,
+                  '# of Homeless Counts': 1 },
+                { ID: 19982,
+                  Name: 'Liam Smith',
+                  Source: 'Homeless',
+                  'Total Duration': 7,
+                  'Total Counts': 3,
+                  '# of Jail Days': 0,
+                  '# of Jail Counts': 0,
+                  '# of Homeless Days': 7,
+                  '# of Homeless Counts': 3 },
+                { ID: 19932,
+                  Name: 'Griffin Smith',
+                  Source: 'Homeless',
+                  'Total Duration': 3,
+                  'Total Counts': 2,
+                  '# of Jail Days': 0,
+                  '# of Jail Counts': 0,
+                  '# of Homeless Days': 3,
+                  '# of Homeless Counts': 2 },
       ],
       bar_data: [
-        [{x: 'Jail', y: 40}, {x: 'Jail & Homeless', y: 30}],
-        [{x: 'Jail', y: 20}, {x: 'Jail & Homeless', y: 30}],
-        [{x: 'Jail', y: 30}, {x: 'Jail & Homeless', y: 20}],
-        [{x: 'Jail', y: 5}, {x: 'Jail & Homeless', y: 10}],
-        [{x: 'Jail', y: 5}, {x: 'Jail & Homeless', y: 10}]
+        [{x: 'Jail', y: 40}, {x: 'Homeless', y: 20}, {x: 'Jail & Homeless', y: 30}],
+        [{x: 'Jail', y: 20}, {x: 'Homeless', y: 15}, {x: 'Jail & Homeless', y: 30}],
+        [{x: 'Jail', y: 30}, {x: 'Homeless', y: 25}, {x: 'Jail & Homeless', y: 20}],
+        [{x: 'Jail', y: 5}, {x: 'Homeless', y: 30}, {x: 'Jail & Homeless', y: 10}],
+        [{x: 'Jail', y: 5}, {x: 'Homeless', y: 10}, {x: 'Jail & Homeless', y: 10}],
       ]
     }
   }
@@ -56,7 +154,7 @@ export default class Charts extends React.Component {
       <div style={styles.page}>
         <h2>Charts</h2>
         <div style={styles.container}>
-          <Card style={styles.card}>
+          <Card style={styles.venn}>
             <CardTitle title="Venn Diagram" titleStyle={{'font-size': 22}} />
             <Venn data={this.state.data} />
           </Card>
@@ -71,11 +169,13 @@ export default class Charts extends React.Component {
             </div>
             <TableList data={this.state.table_data} />
           </Card>
-          <Card style={styles.card}>
+          <Card style={styles.bar_chart}>
             <CardTitle title="Duration Bar Chart" titleStyle={{'font-size': 22}} />
             <DurationBarChart data={this.state.bar_data} />
           </Card>
         </div>
+        <RaisedButton label="Download List" secondary={true} style={styles.button} />
+        <RaisedButton label="Download Chart" secondary={true} style={styles.button} />
       </div>
     )
   }
