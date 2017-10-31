15dbcfae864f864df1b8911d391ccdc957eee483
Adolfo De Unánue <nanounanue@gmail.com>
2017-10-31 13:35:31 -0500

    Closes #41

---
+++ matcher/matcher.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+
+import logging
+
+import pandas as pd
+
+from typing import List
+
+
+def select_columns(df, columns_to_select: List):
+    pass
+
+def indexing(df):
+    pass
+
+def deduplicate(df):
+    pass
+
+
+def run(df, columns_to_select):
+    return deduplicate(indexing(select_columns(df, columns_to_select)))
+
+
+if __name__ == "main":
+    df = None
+    columns_to_select = None
+
+    
+    run(df, columns_to_select)
