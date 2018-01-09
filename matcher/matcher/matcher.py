4960d706db2c8735439f24639ba86b781725f120
Erika Salomon <ecsalomon@gmail.com>
2018-01-09 16:32:25 +0000

    add comments, cleanup logging

--- matcher/matcher/matcher.py
+++ matcher/matcher/matcher.py
@@ -69,6 +69,10 @@ def cluster(
     leaf_size:int=30,
     n_jobs:int=1
 ) -> pd.DataFrame:
+    """ Cluster the scored entities into individuals. Return the cluster ids
+    indexed with the source row_id.
+    """
+
     logger.info('Beginning clustering.')
     df = 1 - df
     clusterer = DBSCAN(
@@ -81,11 +85,10 @@ def cluster(
         p=None,
         n_jobs=n_jobs
     )
-    logger.warning(df)
+
     clusterer.fit(X=df)
-    logger.info('Clustering done!')
-    logger.warning(clusterer.core_sample_indices_)
-    logger.warning(clusterer.labels_)
+    logger.info('Clustering done! Assigning matched ids.')
+
     return pd.Series(
         index=df.index,
         data=clusterer.labels_
@@ -93,6 +96,16 @@ def cluster(
 
 
 def square_distances(upper_right, df1, df2):
+    """ For now, this function uses recursion to fill in the self matches by
+    relying on exact matches on source_id. For non-self-matches, the source_id
+    will be the matched_id from the previous run, so we will be relying on our
+    previous matches. If two elements share a matched_id, we score them as 1;
+    else, we score them as 0. Once we start caching the distances matrices and
+    storing the match metadata in the output, it may save time to check for the
+    cache and use it if it is available, rather than recursing here (and forcing
+    1 or 0 distances).
+    """
+    logger.info('Expanding distances matrix with self-matches.')
     upper_left = match(
         df1,
         df1.copy(),
@@ -107,6 +120,7 @@ def square_distances(upper_right, df1, df2):
     ).pivot(index='row_id_left', columns='row_id_right', values='matches')
     left = pd.concat([upper_left, upper_right.T])
     right = pd.concat([upper_right, lower_right])
+
     return pd.concat([left, right], axis=1)
 
 
@@ -117,21 +131,26 @@ def generate_matched_ids(
     clustering_params:dict,
     self_match
 ) -> tuple:
-    logger.info('Generating matched ids.')
+    logger.info('Beginning clustering & id generation.')
     n = len(df1)
     m = len(df2)
 
-    if not self_match:    
+    # for clustering, the distances must be a square matrix with all possible
+    # pairs. if we are doing a self-match, this is what we have, but if we are
+    # matching different data sources, we only have the upper right portion of
+    # the matrix (the distances between elements in the different sets), and we
+    # need to complete the square with the self-matches for each matrix, plus
+    # the transpose of the distances we have (for the lower left portion)
+    if not self_match:
         distances = square_distances(distances, df1, df2)
 
     ids = cluster(
         distances, **clustering_params
     )
-    logging.warning(ids)
-    logging.warning(ids.head(n))
-    logging.warning(ids.tail(m))
+
     df1['matched_id'] = ids.head(n)
     df2['matched_id'] = ids.tail(m)
+
     logger.info('Matched ids generated')
 
     return (df1, df2)
@@ -149,6 +168,7 @@ def run(
     
     df1 = utils.generate_row_ids(df1)
     df1['source_id'] = utils.get_source_id(df1)
+    
     if df2 is None:
         df2 = df1.copy()
         self_match = True
