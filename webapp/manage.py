f2be6e94a2153538251b9eab1f6440527cc72f86
tweddielin <tweddielin@gmail.com>
2018-02-23 17:45:48 -0600

    import get_redis_connection()

--- webapp/manage.py
+++ webapp/manage.py
@@ -2,13 +2,13 @@ import redis
 from flask_script import Server, Manager
 from rq import Connection, Worker
 from webapp import app
-from webapp.apis.upload import redis_connection
+from webapp.apis.upload import get_redis_connection
 
 manager = Manager(app)
 
 @manager.command
 def runworker():
-    with Connection(redis_connection):
+    with Connection(get_redis_connection()):
         worker = Worker(['webapp'])
         worker.work()
 
