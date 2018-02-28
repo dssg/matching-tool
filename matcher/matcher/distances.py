09cb4b84429d8ff751f9258f2de063d477ca0cd5
Adolfo De Unánue <nanounanue@gmail.com>
2018-02-28 14:44:04 -0600

    Transformer with ngrams, unite columns and split columns. Function signature for the new distances

--- matcher/matcher/distances.py
+++ matcher/matcher/distances.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from typing import List
 
+# String distances
+
 def exact(df:pd.DataFrame, keys: List) -> pd.DataFrame:
     """
     Calculates the "exact distance" between all the selected columns
@@ -16,4 +18,35 @@ def exact(df:pd.DataFrame, keys: List) -> pd.DataFrame:
      
     return df
 
+def truncate_strings(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
 
+def longest_common_substring(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def levenshtein(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def qgram(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def jaccard(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def jaro_winkler(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+
+# Numerical distances
+
+def mad(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def linear_partial_aggrement(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+def quadratic_partial_aggrement(df:pd.DataFrame, keys: List) -> pd.DataFrame:
+    return df
+
+
+# Date distances
