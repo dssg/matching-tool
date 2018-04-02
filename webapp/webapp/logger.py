12ac6b4c444663569dbfa0f6d6bae182ca4a16bc
tweddielin <tweddielin@gmail.com>
2018-04-02 15:47:06 -0500

    clean up

--- webapp/webapp/logger.py
+++ webapp/webapp/logger.py
@@ -6,7 +6,7 @@ import logging
 import logging.config
 
 
-log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'webapp_logging.ini')
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploader_logging.ini')
 
 logging.config.fileConfig(log_file_path)
 logger = logging.getLogger()
