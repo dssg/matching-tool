1a712d90739ebe099a1add33104e090e9ad06ba2
Eddie Lin <tweddielin@gmail.com>
2018-04-07 11:02:07 -0500

    Uploader logger (#226)
    * logger for uploader
    * use job_id as upload_id
    * replace logging with logger

--- webapp/webapp/app.py
+++ webapp/webapp/app.py
@@ -15,12 +15,6 @@ security = Security(app, user_datastore)
 app.register_blueprint(upload_api)
 app.register_blueprint(chart_api)
 
-@app.before_first_request
-def setup_logging():
-    if not app.debug:
-        # In production mode, add log handler to sys.stderr.
-        app.logger.addHandler(logging.StreamHandler())
-        app.logger.setLevel(logging.DEBUG)
 
 @app.route('/test')
 def test():
