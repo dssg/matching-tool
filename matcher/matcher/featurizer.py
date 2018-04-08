648266b95065d410181ba37e44bf092394dc4492
Erika Salomon <ecsalomon@gmail.com>
2018-04-08 14:32:30 +0000

    rewrite featurizer, add config

--- matcher/matcher/featurizer.py
+++ matcher/matcher/featurizer.py
@@ -3,6 +3,9 @@
 import recordlinkage as rl
 import pandas as pd
 
+from matcher.logger import logger
+
+
 def truncate_string(s:pd.Series, n:int) -> pd.Series:
     if n > 0:
         s = s.astype(str).apply(lambda x: x[:n])
@@ -10,173 +13,68 @@ def truncate_string(s:pd.Series, n:int) -> pd.Series:
         s = s.astype(str).apply(lambda x: x[n:])
     else:
         raise ValueError('I cannot compare strings of 0 length!!!')
-
     return s
 
 
 def compare_exact_n_chars(s1:pd.Series, s2:pd.Series, n:int) -> pd.Series:
     s1 = truncate_string(s1, n)
     s2 = truncate_string(s2, n)
-
     return (s1 == s2).astype(float)
 
 
 def lists_share_any_values(l1:pd.Series, l2:pd.Series) -> pd.Series:
-    # make a pd.DataFrame from the columns, converting the strings to lists
     df = pd.concat([l1, l2], axis=1, keys=['l1','l2'])
-    
-    # for each row, check whether any of the items in the first list is in the second
     return df.apply(lambda row: any(i in row.l2 for i in row.l1), axis=1).astype(float)
 
 
 def lists_share_all_values(l1:pd.Series, l2:pd.Series) -> pd.Series:
-    # make a pd.DataFrame from the columns, converting the strings to lists
     df = pd.concat([l1, l2], axis=1, keys=['l1','l2'])
-    
-    # for each row, check whether all elements of l1 shared with l2 and vice versa
     l1_all = df.apply(lambda row: all(i in row.l2 for i in row.l1), axis=1)
     l2_all = df.apply(lambda row: all(i in row.l1 for i in row.l2), axis=1)
-
     return (l1_all & l2_all).astype(float)
 
 
-def generate_features(pairs:pd.MultiIndex, df:pd.DataFrame) -> pd.DataFrame:
-    compare_cl = rl.Compare()
+class Comparer(Object):
+    def __init__(self):
+        self.comparer = rl.Compare()
 
-    # full_name
-    # full name is only given if name parts are not. maybe we should do some preprocessing on full names to create
-    # name parts and use only the name parts, especially since it is possible for the jail and HMIS systems to
-    # differ on what they use
-    if 'full_name' in df.columns:
-        compare_cl.exact('full_name', 'full_name', label='full_name_exacti_distance')
-        compare_cl.string('full_name', 'full_name', method='jarowinkler', label='full_name_jarowinkler_distance')
-        # compare_cl.string('full_name', 'full_name', method='qgram',  label='full_name_qgram_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'full_name', 'full_name', 5, label='full_name_first_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'full_name', 'full_name', -5, label='full_name_last_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'full_name', 'full_name', 3, label='full_name_first_3_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'full_name', 'full_name', -3, label='full_name_last_3_distance')
+    def compare_exact(self, col_name:str, n_chars:int=None):
+        if n_chars:
+            logger.debug(f'Doing an exact comparison of {n_chars} characters of {col_name}')
+            self.comparer.exact(col_name, col_name, label=f'{col_name}_exact_distance')
+        else:
+            logger.debug(f'Doing an exact comparison of {col_name}')
+            self.comparer.compare_vectorized(compare_exact_n_chars, col_name, col_name, n, label=f'{col_name}_exact_{n_chars}_distance')
+    
+    def compare_string_distance(self, col_name:str, method:str):
+        logger.debug(f'Doing a {method} comparison of {col_name}')
+        self.comparer.string(col_name, col_name, method=method, label=f'{col_name}_{method}_distance')
 
-    # prefix
-    # we should preprocess prefixes to remove punctuation and possibly spaces
-    if 'prefix' in df.columns:
-        compare_cl.exact('prefix', 'prefix', label='prefix_exact_distance')
-        compare_cl.string('prefix', 'prefix', method='jaro',  label='prefix_jaro_distance')
+    def compare_swap_month_days(self, col_name:str, swap_score:float=None):
+        logger.debug(f'Checking if the month and day are swapped in {col_name}')
+        self.comparer.date(col_name, col_name, swap_month_day=swap_score, swap_months=None)
 
-    # first_name
-    # potential preprocessing steps:
-    # - remove punctuation
-    # - create: full_first_name, first_word_first_name
-    # - try using second+ word of first name as middle name if no middle name 
-    if 'first_name' in df.columns:
-        compare_cl.exact('first_name', 'first_name', label='first_name_exact_distance')
-        compare_cl.string('first_name', 'first_name', method='jarowinkler',  label='first_name_jarowinkler_distance')
-        # compare_cl.string('first_name', 'first_name', method='qgram',  label='first_name_qgram_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'first_name', 'first_name', 5, label='first_name_first_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'first_name', 'first_name', -5, label='first_name_last_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'first_name', 'first_name', 3, label='first_name_first_3_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'first_name', 'first_name', -3, label='first_name_last_3_distance')
+    def compare_numeric_distance(self, col_name:str, **kwargs):
+        logger.debug(f'Doing a numeric distance calculation on {col_name}')
+        self.comparer.date(col_name, col_name, kwargs)
 
-    # middle_name
-    # potential preprocessing steps:
-    # - remove punctuation
-    # - create: full_middle_name, first_word_middle_name, second_word_middle_name
-    if 'middle_name' in df.columns:
-        compare_cl.exact('middle_name', 'middle_name', label='middle_name_exact_distance')
-        compare_cl.string('middle_name', 'middle_name', method='jarowinkler',  label='middle_name_jarowinkler_distance')
-        # compare_cl.string('middle_name', 'middle_name', method='qgram',  label='middle_name_qgram_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'middle_name', 'middle_name', 5, label='middle_name_first_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'middle_name', 'middle_name', -5, label='middle_name_last_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'middle_name', 'middle_name', 3, label='middle_name_first_3_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'middle_name', 'middle_name', -3, label='middle_name_last_3_distance')
+    def compare_list(self, col_name:str, method:str)
+        if method == 'any':
+            logger.debug(f'Checking if {col_name} shares any value.')
+            self.comparer.compare_vectorized(lists_share_any_values, col_name, col_name, label=f'{col_name}_any_list_item_distance')
+        elif method == 'all':
+            logger.debug(f'Checking if {col_name} shares all values.')
+            self.comparer.compare_vectorized(lists_share_all_values, col_name, col_name, label=f'{col_name}_all_list_items_distance')
+        else:
+            raise ValueError(f"I don't know how to compare lists with this method ({method}). Please send me 'all' or 'any'.")
 
-    # last_name
-    # potential preprocessing steps:
-    # - remove punctuation
-    # - create: full_last_name, first_word_last_name, second_word_last_name
-    if 'last_name' in df.columns:
-        compare_cl.exact('last_name', 'last_name', label='last_name_exact_distance')
-        compare_cl.string('last_name', 'last_name', method='jarowinkler',  label='last_name_jarowinkler_distance')
-        # compare_cl.string('last_name', 'last_name', method='qgram',  label='last_name_qgram_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'last_name', 'last_name', 5, label='middle_name_first_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'last_name', 'last_name', -5, label='middle_name_last_5_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'last_name', 'last_name', 3, label='middle_name_first_3_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'last_name', 'last_name', -3, label='middle_name_last_3_distance')
 
-    # suffix
-    if 'suffix' in df.columns:
-        # we should preprocess suffixes to remove punctuation and possibly spaces
-        compare_cl.exact('suffix', 'suffix', label='suffix_exact_distance')
-        compare_cl.string('suffix', 'suffix', method='jaro',  label='suffix_jaro_distance')
+def generate_features(all_comparisons:dict, pairs:pd.MultiIndex, df:pd.DataFrame) -> pd.DataFrame:
+    c = Comparer() 
+    for col_name, comparisons in self.all_comparisons:
+        for comparison, args in comparison:
+            method_to_call = getattr(c.comparer, comparison)
+            method_to_call(col_name, args)
 
-    # dob
-    # MUST BE CAST TO DATETIME DURING PREPROCESSING
-    if 'dob' in df.columns:
-        compare_cl.exact('dob', 'dob', label='dob_exact_distance')
-        compare_cl.date('dob', 'dob', label='dob_date_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'dob', 'dob', 4, label='dob_year_exact_distance')
+    return c.comparer.compute(pairs, df)
 
-    # ssn
-    # THIS SHOULD BE CONVERTED TO STRING. The SSN consists of 3 words, and numerical distances are only
-    # VAGUELY meaningful (e.g., the first 3 digits increase roughly east to west but not in a rigorous way,
-    # and the second 2 digits are given out in a fixed but non-monotonic order)
-    # the first three digits are the "area code" of where the person was registered.
-    # most people living in an area will have one of a few local area codes; therefore, the distinctiveness
-    # of the area code may be useful for matching. we may want to preprocess ssn to extract the area code
-    # to make this comparison.
-    if 'ssn' in df.columns:
-        compare_cl.exact('ssn', 'ssn', label='ssn_exact_distance')
-        compare_cl.string('ssn', 'ssn', method='jaro',  label='ssn_jaro_distance')
-        # compare_cl.string('ssn', 'ssn', method='qgram',  label='ssn_qgram_distance')
-        # the last four digits are the "serial number" of the person (i.e., the most unique part of the number) 
-        # and are often the only known/shared component, so we will run a special exact comparison on them
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'ssn', 'ssn', -4, label='ssn_last_4_distance')
-
-    # ssn_hash
-    # we can only compare these exact
-    if 'ssn_hash' in df.columns:
-        compare_cl.exact('ssn_hash', 'ssn_hash', label='ssn_hash_exact_distance')
-
-    # dmv_number
-    # THIS SHOULD BE CAST TO STRING. In some jurisdictions, they are strings and in others ints. To ensure
-    # that we can generalize here, we need to convert to string for all of them.
-    if 'dmv_number' in df.columns:
-        compare_cl.exact('dmv_number', 'dmv_number', label='dmv_number_exact_distance')
-        compare_cl.string('dmv_number', 'dmv_number', method='jaro',  label='dmv_number_jaro_distance')
-        # compare_cl.string('dmv_number', 'dmv_number', method='qgram',  label='dmv_number_qgram_distance')
-
-    # dmv_state
-    if 'dmv_state' in df.columns:
-        compare_cl.exact('dmv_state', 'dmv_state', label='dmv_state_exact_distance')
-
-    # race
-    # if race is not useful, we may want to consider some preprocessing around that various codes for
-    # missingness (refused, doesn't know, NULL, etc.)
-    # race codes are single characters but can be combined in a list. we will break it down and see if
-    # (a) all races match, disregarding order;
-    # (b) any race matches
-    if 'race' in df.columns:
-        compare_cl.compare_vectorized(lists_share_all_values, 'race', 'race', label='race_all_distance')
-        compare_cl.compare_vectorized(lists_share_any_values, 'race', 'race', label='race_any_distance')
-
-    # ethnicity
-    # ethnicity encodes only Hispanic/Not Hispanic. for some databases, Hispanic is actually included
-    # in the race categories instead of in a separate field. we may want to do some pre-processing to
-    # to add H to the race list where the ethnicity field contains 'Hispanic'
-    if 'ethnicity' in df.columns:
-        compare_cl.exact('ethnicity', 'ethnicity', label='ethnicity_exact_distance')
-
-    # sex
-    # we accept 'TM' and 'TF', which are used primarily by HMIS systems to identify trans men and women.
-    # in most criminal justice databases, there is no separate designation for trans men and women or
-    # non-binary genders, and sex/gender may be entered based on self-identification, official state id,
-    # or biological sex as determined by jail staff. so we will compare both the full value and just the
-    # last letter
-    if 'sex' in df.columns:
-        compare_cl.exact('sex', 'sex', label='sex_exact_distance')
-        compare_cl.compare_vectorized(compare_exact_n_chars, 'sex', 'sex', -1, label='sex_exact_last_letter_distance')
-
-    # we will ignore address info for now, as it is especially unlikely to be useful for this problem
-    # (e.g., maybe we only want to compare addresses from similar timeframes)
-
-    # finally, generate all the comparisons and return the features
-    return compare_cl.compute(pairs, df)
