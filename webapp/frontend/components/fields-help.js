667ef4076d19a66b8528b6abbaf782956b5afb34
Tristan Crockett <thcrockett@uchicago.edu>
2018-05-31 21:10:57 +0000

    Show descriptions for results fields [Resolves #314]

---
+++ webapp/frontend/components/fields-help.js
@@ -0,0 +1,18 @@
+export default (props) => {
+  return (
+    <div>
+      <p><b>matched_id</b> The identifier generated for this person by the most recent matching process. Will not be stable across runs of the matcher</p>
+      <p><b>jail_id</b> The person identifier from the uploaded jail bookings dataset</p>
+      <p><b>hmis_id</b> The person identifier from the uploaded hmis service stays dataset</p>
+      <p><b>first_name</b> The first name of the person, picked by a stringwise maximum function of all the first names present in events for that person</p>
+      <p><b>last_name</b> The last name of the person, picked by a stringwise maximum function of all the last names present in events for that person</p>
+      <p><b>last_jail_contact</b> The most recent jail entry date for any booking event associated with this person</p>
+      <p><b>last_hmis_contact</b> The most recent client location start date for any HMIS service stay associated with this person</p>
+      <p><b>jail_contact</b> The number of jail bookings associated with this person whose jail entry or exit date is within the specified time range</p>
+      <p><b>hmis_contact</b> The number of HMIS service stays associated with this person whose client location start or end date is within the specified time range</p>
+      <p><b>total_contact</b> The number of jail bookings and HMIS service stays associated with this person whose start or end date is within the specified time range</p>
+      <p><b>cumu_jail_days</b> The cumulative number of days associated with all jail bookings for this person whose start or end date is within the specified time range. All days associated with the booking will be counted, even if the booking is only partially within the specified time range.  If the booking had not yet ended as of upload time, the upload time is used as the end time.</p>
+      <p><b>cumu_hmis_days</b> The cumulative number of days associated with all HMIS service stays for this person whose start or end date is within the specified time range. All days associated with the stay will be counted, even if the stay is only partially within the specified time range.  If the stay had not yet ended as of upload time, the upload time is used as the end time.</p>
+    </div>
+  )
+}
