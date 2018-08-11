b9081b4a280b23b6a7b991ec912b439d06369df0
tweddielin <tweddielin@gmail.com>
2018-08-10 21:34:04 -0500

    rename webapp in scripts and docs

--- docs/sources/dev/webapp.md
+++ docs/sources/dev/webapp.md
@@ -1,1 +1,32 @@
 # Modifying the Webapp
+
+The webapp can be modified for adding new APIs, plots or functionalities that will improve the usability as well as
+the analysis. This document introduce tech stacks and the code structure in the matching-tool. The web application
+of this matching tool follows one of the popular and productive tech stacks today, a combination of [React](https://reactjs.org/)/[Redux](https://github.com/reduxjs/react-redux) for the frontend, [Python/Flask](http://flask.pocoo.org/) for the backend, and [SQLAlchemy](https://www.sqlalchemy.org/) as the Python Object Relation Map (ORM) to communicate with [PostgreSQL](https://www.postgresql.org/) database.
+
+## Database
+
+- PostgreSQL
+
+## Backend
+
+1. Tech stacks
+	- Flask
+	- goodtables
+	- redis/rq
+	- sqlalchemy
+	- alembic
+
+2. Code Structure
+
+## Frontend
+
+1. Tech stacks
+	- React
+	- Redux
+	- Material-UI
+	- react-vis
+	- venn.js
+	- ramda
+
+2. Code Structure
--- scripts/development/create_test_users.sh
+++ scripts/development/create_test_users.sh
@@ -1,13 +1,13 @@
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask users create --password password -a testuser@example.com
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles create test_hmis_service_stays
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles create test_jail_bookings
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles add testuser@example.com test_hmis_service_stays
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles add testuser@example.com test_jail_bookings
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask users create --password password -a testuser@example.com
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles create test_hmis_service_stays
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles create test_jail_bookings
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles add testuser@example.com test_hmis_service_stays
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles add testuser@example.com test_jail_bookings
 
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask users create --password password -a countyone@example.com
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles create countyone_hmis
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles add countyone@example.com countyone_hmis
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask users create --password password -a countyone@example.com
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles create countyone_hmis
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles add countyone@example.com countyone_hmis
 
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask users create --password password -a countytwo@example.com
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles create countytwo_hmis
-PYTHONPATH='./webapp' FLASK_APP=webapp/webapp/app.py flask roles add countytwo@example.com countytwo_hmis
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask users create --password password -a countytwo@example.com
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles create countytwo_hmis
+PYTHONPATH='./webapp' FLASK_APP=webapp/backend/app.py flask roles add countytwo@example.com countytwo_hmis
